9b713f0e6e29cb3ab6c3ad20e5e5a4773ba189d3
IlyaBulatau <ilyabulatau@gmail.com>
2024-05-26 19:56:36 +0300

    ref index endpoint, added get chat by id endpoint

--- chat/chats/endpoints.py
+++ chat/chats/endpoints.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from auth.decorators import login_required
+from chats.use_cases.chats import get_chats_by_id, get_chats_list
+from core.dependens import get_current_user
+from core.domains import Chat, User
 from settings import BASE_DIR
 
 
@@ -13,5 +16,14 @@ templates = Jinja2Templates(BASE_DIR.joinpath("templates"))
 
 @router.get("/", response_class=HTMLResponse)
 @login_required
-async def index(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+async def index(request: Request, user: User = Depends(get_current_user), chats: list[Chat] = Depends(get_chats_list)):
+    return templates.TemplateResponse(request=request, name="index.html", context={"user": user, "chats": chats})
+
+
+@router.get("/{chat_id}", response_class=HTMLResponse)
+@login_required
+async def get_chat_by_id(
+    request: Request,
+    chat: Chat = Depends(get_chats_by_id),
+):
+    return templates.TemplateResponse(request=request, name="chat.html", context={"chat": chat})
