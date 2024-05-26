985892e25f9c4c7ebc1666da789fac3755a4c1b7
IlyaBulatau <ilyabulatau@gmail.com>
2024-05-26 16:45:16 +0300

    generated migrations add domains model chats, messages

--- chat/core/domains.py
+++ chat/core/domains.py
@@ -6,4 +6,25 @@ class User:
     id: int
     username: str
     email: str
+    chats: list["Chat"]
+    messages: list["Message"]
     password: str | None = None
+
+
+@dataclass
+class Chat:
+    id: int
+    title: str
+    owner_id: int
+    users: list["User"]
+    messages: list["Message"]
+
+
+@dataclass
+class Message:
+    id: int
+    text: str
+    user_id: int
+    chat_id: int
+    user: User
+    chat: Chat
