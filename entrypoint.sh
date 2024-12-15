34c95a6724c56794c99e045062725c0169716d96
IlyaBulatau <ilyabulatau@gmail.com>
2024-12-15 21:06:04 +0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=`pwd`
 python3 cli/migrate.py
-uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers
+uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded_allow_ips=*
