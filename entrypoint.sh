4edb50106b1778da068245a63707f0889322a405
IlyaBulatau <ilyabulatau@gmail.com>
2024-12-18 22:21:08 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=`pwd`
 python3 cli/migrate.py
-uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips=* --log-config ./log_config.yaml --log-level info
+uvicorn main:app --host 0.0.0.0 --port 8000 --log-config ./log_config.yaml --log-level info --proxy-headers --forwarded-allow-ips=*
